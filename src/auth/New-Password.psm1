da7324bfef4bf79742629dc4e2066f502a6ec147
Jacob Ochoa <Jacob.Ochoa@gmail.com>
2018-05-22 11:59:03 -0700

    fixing new-password typo

--- src/auth/New-Password.psm1
+++ src/auth/New-Password.psm1
@@ -2,32 +2,28 @@
 
 
 
-Function New-Password ($length = 8 ) {
+Function New-Password ([int]$MinLength = 8 ) {
+    
     DO {
         #GEN PASSWORD
-        $Password = [System.Web.Security.Membership]::GeneratePassword( $length, 2)
-        
+        $Password = [System.Web.Security.Membership]::GeneratePassword( $MinLength, 2)
+
         # AD PASSWORD REQUIREMENTS
+        $LENGTH  = [regex]::new(".{$MinLength,}").Match( $Password )
         $SPECIAL = [regex]::new('[!@#$%<>^&?]').Match( $Password )
         $LOWER   = [regex]::new('[a-z]').Match( $Password )
         $UPPER   = [regex]::new('[A-Z]').Match( $Password )
         $NUMBER  = [regex]::new('[0-9]').Match( $Password )
-        $LENGTH  = [regex]::new(".{$length,}").Match( $Password )
         
         # TEST PASSWORD
         $Meets_Complexity = $true
+        if (!$LENGTH.Success)  {$Meets_Complexity = $false}
         if (!$SPECIAL.Success) {$Meets_Complexity = $false}
         if (!$LOWER.Success)   {$Meets_Complexity = $false}
         if (!$UPPER.Success)   {$Meets_Complexity = $false}
         if (!$NUMBER.Success)  {$Meets_Complexity = $false}
-        if (!$LENGTH.Success)  {$Meets_Complexity = $false}
     }
     Until($Meets_Complexity)
 
     return $Password
 }
-
-
-
-
-
